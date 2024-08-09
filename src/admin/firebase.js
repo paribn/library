fc29b1141fecac90f473ba0497854567dabb98e4
perinov <novruzovaperi2@gmail.com>
2024-08-10 02:50:33 +0400

    firebase added

--- src/admin/firebase.js
+++ src/admin/firebase.js
@@ -26,15 +26,19 @@ const app = initializeApp(firebaseConfig);
 
 const db = getDatabase(app);
 
-function writeUserData(name, password) {
-  set(ref(db, "users/"), {
-    username: name,
-    password: password,
-  });
+// function writeUserData(name, password) {
+//   set(ref(db, "users/"), {
+//     username: name,
+//     password: password,
+//   });
+// }
+// writeUserData("admin", "adminpas");
+
+function writeBasicData(dbName, data) {
+  set(ref(db, dbName), data);
 }
 
-writeUserData("admin", "adminpas");
-
+// get single  data from db. cannot use it if you have more than one data
 export async function getData(dbName) {
   try {
     let data = await get(ref(db, dbName));
@@ -46,4 +50,39 @@ export async function getData(dbName) {
   } catch (error) {
     console.log(error);
   }
+}
+
+// it will return all data from db. example usage : readPushedData("books/"); IT CAN BE USED FOR WHICH DATA STORED WITH PUSH METHOD
+export async function readPushedData(dbName) {
+  try {
+    const snapshot = await get(ref(db, dbName));
+    if (snapshot.exists()) {
+      let newData = Object.entries(snapshot.val()).map((item) => ({
+        id: item[0],
+        ...item[1],
+      }));
+      return newData;
+    } else {
+      console.log("No data available");
+      return [];
+    }
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+}
+
+// delete data from db. example usage : deleteData("books/-N7cXQ2zQjIYy9WQ7w3x");
+export function deleteData(dbTable) {
+  remove(ref(db, dbTable));
+}
+
+// update existing data example : updateData("books/", "-N7cXQ2zQjIYy9WQ7w3x", {name : "book1", author : "author1"});
+export function updateData(dbTable, dataID, data) {
+  update(ref(db, dbTable + dataID), data);
+}
+
+// push data with unic id  data must be in json format example : pushData("books/", {name : "book1", author : "author1"});
+export function pushData(dbTable, data) {
+  push(ref(db, dbTable), data);
 }
